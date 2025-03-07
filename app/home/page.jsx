cdffd05f69e9c4729a56d0d1da9da0005563b157
pablodev <pabloteixeira591@gmail.com>
2025-03-07 07:17:25 -0300

    adding tech icons starting feedback section

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -212,29 +212,87 @@ export default function Home() {
           <span className="text-blue-700">work</span> with
         </h1>
         <div className="flex flex-col border-1 border-gray-400 max-w-[500px] items-center rounded-smright-260 ">
-            {/* Card picture */}
-            <div className="border-1 border-gray-400 flex justify-between p-2 w-full items-center">
-              <span>Techs</span>
-              <div>
-                <ul className="flex items-center space-x-4">
-                  <li>
-                    <Minus size={15} />
-                  </li>
-                  <li>
-                    <Square size={15} />
-                  </li>
-                  <li>
-                    <X size={15} />
-                  </li>
-                </ul>
-              </div>
-            </div>
-            <div className="p-4 flex flex-col space-y-4">
-              <div>
-                <Image src={'/next.svg'} alt="next icon" width={50} height={50}/>
-              </div>
+          {/* Card picture */}
+          <div className="border-1 border-gray-400 flex justify-between p-2 w-full items-center">
+            <span>Techs</span>
+            <div>
+              <ul className="flex items-center space-x-4">
+                <li>
+                  <Minus size={15} />
+                </li>
+                <li>
+                  <Square size={15} />
+                </li>
+                <li>
+                  <X size={15} />
+                </li>
+              </ul>
             </div>
           </div>
+          <div className="p-4 flex flex-wrap gap-5 items-center justify-center">
+            <div className="flex items-center space-x-4">
+              <Image src={"/next.svg"} alt="next icon" width={30} height={30} />
+              <p className="font-medium text-xl text-gray-400">Next</p>
+            </div>
+            <div className="flex items-center space-x-4">
+              <Image
+                src={"/react.svg"}
+                alt="react icon"
+                width={30}
+                height={30}
+              />
+              <p className="font-medium text-xl text-gray-400">React</p>
+            </div>
+            <div className="flex items-center space-x-4">
+              <Image
+                src={"/tailwind.svg"}
+                alt="Tailwind icon"
+                width={30}
+                height={30}
+              />
+              <p className="font-medium text-xl text-gray-400">Tailwind</p>
+            </div>
+            <div className="flex items-center space-x-4">
+              <Image
+                src={"/tailwind.svg"}
+                alt="Tailwind icon"
+                width={30}
+                height={30}
+              />
+              <p className="font-medium text-xl text-gray-400">Tailwind</p>
+            </div>
+            <div className="flex items-center space-x-4">
+              <Image
+                src={"/javascript.svg"}
+                alt="Javascript icon"
+                width={30}
+                height={30}
+              />
+              <p className="font-medium text-xl text-gray-400">JS</p>
+            </div>
+            <div className="flex items-center space-x-4">
+              <Image
+                src={"/firebase.svg"}
+                alt="Firebase icon"
+                width={30}
+                height={30}
+              />
+              <p className="font-medium text-xl text-gray-400">Firebase</p>
+            </div>
+          </div>
+        </div>
+        <p className="text-gray-400">
+          *I use other tools, mainly animation tools, but I decided to put the
+          main ones in this container.
+        </p>
+      </div>
+      <div className="min-h-screen overflow-x-hidden relative flex items-center flex-col space-y-20">
+        <span className="bg-blue-700 h-[100px] w-[1px]"></span>
+
+        <h1 className="text-center text-3xl">
+          <span className="text-blue-700">Nice</span> things people say about me and my
+          work
+        </h1>
       </div>
     </div>
   );

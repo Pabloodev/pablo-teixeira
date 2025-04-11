657f4d04f4d97f6ab840a9013fc10632f361e67b
pablodev <pabloteixeira591@gmail.com>
2025-04-11 08:42:42 -0300

    update

--- app/data/feedbacks.js
+++ app/data/feedbacks.js
@@ -1,10 +1,16 @@
 export const feedbacks = [
   {
-    avatar: "./",
+    avatar: "/vitoria.jpg",
     content: "adsada",
     name: "Marlon Oliveira",
     role: "Networks cordinator"
-  }
+  },
+  {
+    avatar: "/vitoria.jpg",
+    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit Sapiente dolorum accusamus molestiae illum quae itaque numquam magni doloremque eaque sit tempore, corrupti voluptatibus necessitatibus! Delectus necessitatibus sint fuga assumenda dolor.",
+    name: "Marlon Oliveira",
+    role: "Networks cordinator"
+  },
 ]
 
 // Tenho que preencher esse script com o que falta
--- app/ui/components/Feedback.jsx
+++ app/ui/components/Feedback.jsx
@@ -1,44 +1,38 @@
-import { Minus, Square, X, ArrowRight, Mouse } from "lucide-react";
 import Image from "next/image";
-import Link from "next/link";
+import { feedbacks } from "@/app/data/feedbacks";
 
-export default function Feedback(){
+export default function Feedback() {
   return (
     <div className="min-h-screen overflow-x-hidden relative flex items-center flex-col space-y-20">
-        <h1 className="text-center text-3xl">
-          <span className="text-red-700">Nice</span> things people say about me
-          and my work
-        </h1>
+      <h1 className="text-center text-3xl">
+        <span className="text-red-700">Nice</span> things people say about me
+        and my work
+      </h1>
 
-        <div className="flex flex-col border-1 border-gray-400 max-w-[600px] items-center rounded-sm right-130 top-30">
-          <div className="border-1 border-gray-400  flex justify-between p-2 w-full items-center">
-            <span>Testimonial</span>
-          </div>
-          <div className="flex flex-col space-y-4 p-8">
+      <p className="text-center"></p>
+
+      <div className="flex items-center rounded-sm gap-10">
+        {feedbacks.map((feed, index) => (
+          <div
+            key={index}
+            className="w-[500px] h-[300px] border-1 border-gray-400 flex flex-col space-y-4 p-8"
+          >
             <div className="flex items-center space-x-4">
               <Image
                 className="rounded-full"
-                src={"/vitoria.jpg"}
+                src={feed.avatar}
                 width={60}
                 height={60}
                 alt="Avatar Testimonial"
               />
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Sapiente dolorum accusamus molestiae illum quae itaque numquam
-                magni doloremque eaque sit tempore, corrupti voluptatibus
-                necessitatibus! Delectus necessitatibus sint fuga assumenda
-                dolor.
-              </p>
+              <p>{feed.content}</p>
             </div>
             <p className="text-orange-200">
-              Vitória Lopes{" "}
-              <span className="text-gray-400">
-                // Technical sector supervisor
-              </span>
+              {feed.name} <span className="text-gray-400">// {feed.role}</span>
             </p>
           </div>
-        </div>
+        ))}
       </div>
-  )
+    </div>
+  );
 }
